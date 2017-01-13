6ff9f1218001329617a90020d4dd1e6a6effc490
yazid <yazid@aksaramaya.com>
2017-01-13 16:19:27 +0700

    membuat variabel

--- app/src/main/java/yzd/nt/testingjenkins/MainActivity.java
+++ app/src/main/java/yzd/nt/testingjenkins/MainActivity.java
@@ -11,6 +11,8 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+    String testing;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
